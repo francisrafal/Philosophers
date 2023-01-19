a57bf77bac6db605786bfdc95dea482aaa179a81
Francis Rafal <francis.rafal@gmail.com>
2023-01-19 16:51:58 +0100

    feat: Timestamps

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: frafal <frafal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 11:12:03 by frafal            #+#    #+#             */
-/*   Updated: 2023/01/19 16:17:42 by frafal           ###   ########.fr       */
+/*   Updated: 2023/01/19 16:51:08 by frafal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,19 @@ t_data	*init_data(int argc, char **argv)
 	return (philo);
 }
 
+unsigned long	get_timestamp_in_ms(t_data *philo)
+{
+	unsigned long	sec_elapsed;
+	unsigned long	usec_elapsed;
+	unsigned long	msec_elapsed;
+
+	gettimeofday(&(philo->tv1), NULL);
+	sec_elapsed = philo->tv1.tv_sec - philo->tv0.tv_sec;
+	usec_elapsed = philo->tv1.tv_usec - philo->tv0.tv_usec;
+	msec_elapsed = sec_elapsed * 1000 + usec_elapsed / 1000;
+	return (msec_elapsed);
+}
+
 int	main(int argc, char **argv)
 {
 	t_data	*philo;
@@ -52,7 +65,12 @@ int	main(int argc, char **argv)
 	if (!argc_correct(argc))
 		return (1);
 	philo = init_data(argc, argv);
+	gettimeofday(&(philo->tv0), NULL);
 	if (philo == NULL)
 		return (1);
+	usleep(10000);
+	printf("timestamp: %ld ms\n", get_timestamp_in_ms(philo));
+	usleep(1000000);
+	printf("timestamp: %ld ms\n", get_timestamp_in_ms(philo));
 	return (0);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: frafal <frafal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 11:13:11 by frafal            #+#    #+#             */
-/*   Updated: 2023/01/19 15:21:46 by frafal           ###   ########.fr       */
+/*   Updated: 2023/01/19 16:43:01 by frafal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,17 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdio.h>
+# include <sys/time.h>
 
 typedef struct s_data
 {
-	int	num;
-	int	ttd;
-	int	tte;
-	int	tts;
-	int	eat_times;
+	int				num;
+	int				ttd;
+	int				tte;
+	int				tts;
+	int				eat_times;
+	struct timeval	tv0;
+	struct timeval	tv1;
 }	t_data;
 
 int	ft_atoi(const char *nptr);
