01a8183ddf1d43b09ee423d566af8e99ed57f953
Francis Rafal <francis.rafal@gmail.com>
2023-01-19 16:18:12 +0100

    feat: Parse Arguments

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: frafal <frafal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 11:12:03 by frafal            #+#    #+#             */
-/*   Updated: 2023/01/19 15:04:30 by frafal           ###   ########.fr       */
+/*   Updated: 2023/01/19 16:17:42 by frafal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,33 @@ time_to_sleep \
 	return (1);
 }
 
+t_data	*init_data(int argc, char **argv)
+{
+	t_data	*philo;
+
+	philo = malloc(sizeof (t_data));
+	if (philo == NULL)
+	{
+		printf("malloc fail\n");
+		return (NULL);
+	}
+	philo->num = ft_atoi(argv[1]);
+	philo->ttd = ft_atoi(argv[2]);
+	philo->tte = ft_atoi(argv[3]);
+	philo->tts = ft_atoi(argv[4]);
+	if (argc == 6)
+		philo->eat_times = ft_atoi(argv[5]);
+	return (philo);
+}
+
 int	main(int argc, char **argv)
 {
-	(void)argv;
+	t_data	*philo;
+
 	if (!argc_correct(argc))
+		return (1);
+	philo = init_data(argc, argv);
+	if (philo == NULL)
 		return (1);
 	return (0);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: frafal <frafal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 11:13:11 by frafal            #+#    #+#             */
-/*   Updated: 2023/01/19 15:01:10 by frafal           ###   ########.fr       */
+/*   Updated: 2023/01/19 15:21:46 by frafal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,16 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdio.h>
+
+typedef struct s_data
+{
+	int	num;
+	int	ttd;
+	int	tte;
+	int	tts;
+	int	eat_times;
+}	t_data;
+
+int	ft_atoi(const char *nptr);
 
 #endif
