14b27cf4d5f9b8b41aef2fa775d2420f9d94ef78
Francis Rafal <francis.rafal@gmail.com>
2023-01-19 15:05:12 +0100

    feat: check argc

--- philo/philo.c
+++ philo/philo.c
@@ -6,15 +6,30 @@
 /*   By: frafal <frafal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 11:12:03 by frafal            #+#    #+#             */
-/*   Updated: 2023/01/19 11:25:14 by frafal           ###   ########.fr       */
+/*   Updated: 2023/01/19 15:04:30 by frafal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+int	argc_correct(int argc)
+{
+	if (argc != 5 && argc != 6)
+	{
+		printf("usage: ./philo number_of_philosophers \
+time_to_die \
+time_to_eat \
+time_to_sleep \
+[number_of_times_each_philosopher_must_eat]\n");
+		return (0);
+	}
+	return (1);
+}
+
 int	main(int argc, char **argv)
 {
-	(void)argc;
 	(void)argv;
+	if (!argc_correct(argc))
+		return (1);
 	return (0);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,12 +6,15 @@
 /*   By: frafal <frafal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 11:13:11 by frafal            #+#    #+#             */
-/*   Updated: 2023/01/19 11:13:34 by frafal           ###   ########.fr       */
+/*   Updated: 2023/01/19 15:01:10 by frafal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 # include <pthread.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
 
 #endif
