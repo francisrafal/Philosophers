79b5b3cc36455de88612bbff2b3a24683aba1e1a
Francis Rafal <francis.rafal@gmail.com>
2023-01-31 16:55:59 +0100

    feat: No Die Tests Pass

--- philo/free.c
+++ philo/free.c
@@ -6,7 +6,7 @@
 /*   By: frafal <frafal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 15:30:55 by frafal            #+#    #+#             */
-/*   Updated: 2023/01/30 15:31:39 by frafal           ###   ########.fr       */
+/*   Updated: 2023/01/31 16:46:25 by frafal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,27 +31,14 @@ void	free_forks(t_data *data)
 	free_null(data->forks);
 }
 
-void	free_waiters(t_data *data)
-{
-	int	i;
-
-	i = 0;
-	while (i < data->num_waiters)
-		pthread_mutex_destroy(data->waiters + i++);
-	free_null(data->waiters);
-}
-
 void	free_data(t_data *data)
 {
 	free_null(data->philo_threads);
 	free_forks(data);
-	free_waiters(data);
 	pthread_mutex_destroy(&(data->print_mutex));
 	pthread_mutex_destroy(&(data->tv1_mutex));
 	pthread_mutex_destroy(&(data->alive_mutex));
 	pthread_mutex_destroy(&(data->last_eaten_mutex));
-	pthread_mutex_destroy(&(data->queue_mutex));
-	free_null(data->queue);
 	free_null(data->last_eaten);
 	free_null(data);
 }
--- philo/init.c
+++ philo/init.c
@@ -6,7 +6,7 @@
 /*   By: frafal <frafal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 15:27:43 by frafal            #+#    #+#             */
-/*   Updated: 2023/01/30 16:49:27 by frafal           ###   ########.fr       */
+/*   Updated: 2023/01/31 16:42:29 by frafal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,22 +31,6 @@ t_philo	*init_philosophers(t_data *data)
 	return (philos);
 }
 
-int	init_fork_array(t_data *data)
-{
-	int	i;
-
-	data->fork_array = malloc(data->num * sizeof (int));
-	if (data->fork_array == NULL)
-	{
-		printf("malloc fail\n");
-		return (-1);
-	}
-	i = 0;
-	while (i < data->num)
-        data->fork_array[i] = FORK_FREE;
-	return (0);
-}
-
 t_data	*init_data(int argc, char **argv)
 {
 	t_data	*data;
@@ -66,13 +50,10 @@ t_data	*init_data(int argc, char **argv)
 		data->eat_times = ft_atoi(argv[5]);
 	if (init_forks(data) == -1)
 		return (NULL);
-	if (init_waiters(data) == -1)
-		return (NULL);
 	pthread_mutex_init(&(data->alive_mutex), NULL);
 	pthread_mutex_init(&(data->print_mutex), NULL);
 	pthread_mutex_init(&(data->tv1_mutex), NULL);
 	pthread_mutex_init(&(data->last_eaten_mutex), NULL);
-	pthread_mutex_init(&(data->queue_mutex), NULL);
 	if (init_last_eaten(data) == -1)
 		return (NULL);
 	return (data);
@@ -91,26 +72,6 @@ int	init_forks(t_data *data)
 	i = 0;
 	while (i < data->num)
 		pthread_mutex_init(data->forks + i++, NULL);
-	return (0);
-}
-
-int	init_waiters(t_data *data)
-{
-	int	i;
-
-	if (data->num == 1)
-		data->num_waiters = 1;
-	else
-		data->num_waiters = data->num / 2;
-	data->waiters = malloc(data->num_waiters * sizeof (pthread_mutex_t));
-	if (data->waiters == NULL)
-	{
-		printf("malloc fail\n");
-		return (-1);
-	}
-	i = 0;
-	while (i < data->num_waiters)
-		pthread_mutex_init(data->waiters + i++, NULL);
 	return (0);
 }
 
